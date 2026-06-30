3b81eef598e4d36b6e9b01096cd781de8882ef39
Priyank Jethva <priyank.work09@gmail.com>
2026-06-30 13:00:05 +0530

    Remove leftover environment check file

--- untitled.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import prophet
-import groq
-
-print ("pandas version:", pd.__version__)
-print("prophet version:", pd.__version__)
-print("groq version:", groq.__version__)
-print("\nAll imports succcessful. Environment is ready.")
